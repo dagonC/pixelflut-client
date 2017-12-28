231f784468eb05c2febfb51d2f9afe05d4d6566b
Dagon <none@none.none>
2017-12-28 15:00:45 +0100

    Initial commit, new multithreaded draft

---
+++ pixelflut.py
@@ -0,0 +1,67 @@
+import random
+import sys
+import os.path
+import threading
+import socket
+from PIL import Image
+
+HOST = '151.217.47.77'
+PORT = 8080
+
+def pixel(sock, x, y, r, g, b, a=255):
+    if a == 255:
+        sock.send('PX %d %d %02x%02x%02x\n' % (x, y, r, g, b))
+    else:
+        sock.send('PX %d %d %02x%02x%02x%02x\n' % (x, y, r, g, b, a))
+
+
+def worm(x, y, n, r, g, b):
+    while n:
+        pixel(x, y, r, g, b, 25)
+        x += random.randint(0, 2) - 1
+        y += random.randint(0, 2) - 1
+        n -= 1
+
+
+def writeImage(offsetX, offsetY, im):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    _, _, w, h = im.getbbox()
+    for x in xrange(w):
+        for y in xrange(h):
+            r, g, b = im.getpixel((x, y))
+            pixel(sock, x + offsetX, y + offsetY, r, g, b)
+
+
+def writePixelFlutImage(threadName, im):
+    print(threadName)
+    while True:
+        writeImage(random.randint(0, 924), random.randint(0, 924), im)
+
+
+def launchThreaded(numberOfThreads):
+    threads = []
+    #try:
+    for x in range(0, numberOfThreads):
+        #thread.start_new_thread(writePixelFlutImage, ("Launched thread " + str(x), im,))
+        t = threading.Thread(target=writePixelFlutImage, args=("Launched thread #" + str(x), im))
+        threads.append(t)
+        t.start()
+    #except:
+     #   print("Error: unable to start thread")
+
+if len(sys.argv) != 2:
+    exit
+
+inputFileName = sys.argv[1]
+if inputFileName == '':
+    exit
+print('Sending ' + inputFileName + ' via ' + sys.argv[2] + ' threads')
+
+im = Image.open(inputFileName).convert('RGB')
+im.thumbnail((100, 100), Image.ANTIALIAS)
+#writePixelFlutImage("Bla", im)
+launchThreaded(int(sys.argv[2]))
+
+
+
